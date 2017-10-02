50754a5a255be73f60d9bb887f209f07c3dd1801
doc2git <doc2git@yahoo.com>
2017-10-02 15:10:49 +0800

    1506928249000447370

--- git-init--to--git--gh-pages--deploy.sh
+++ git-init--to--git--gh-pages--deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git init; git add .; git commit -am `date +%s%N`; git remote add all git@github.com:doc2git/vue-router-if-github-support-test.git; git push all master; git branch gh-pages; git checkout gh-pages; git push all gh-pages;
+git init; git add .; git commit -am `date +%s%N`; git remote add all git@github.com:doc2git/$(basename `realpath .`).git; git push all master; git branch gh-pages; git checkout gh-pages; git push all gh-pages;
